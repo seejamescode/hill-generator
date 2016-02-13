3cc875e52c72baf6752b1c6d4f19b5da8d5b8d26
James Y. Rauhut <jamesrauhut@us.ibm.com>
2016-02-13 11:24:48 -0600

    Found missing parameters for GA event

--- hill-generator.js
+++ hill-generator.js
@@ -1,3 +1,11 @@
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-43808769-2', 'auto');
+ga('send', 'pageview');
+
 var obj = {
   who: [
     'A band of clowns',
@@ -68,5 +76,5 @@ function generateHill() {
       }, 500);
     }, 500);
   }, 500);
-  ga('Hill Generator', 'Hill Generated');
+  ga('send', 'event', 'Hill Generator', 'generated');
 };
